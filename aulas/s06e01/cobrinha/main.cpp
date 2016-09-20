13f1728a17bacf98b5822c75a968ca47ea232818
David Sena <sena.ufc@gmail.com>
2016-09-20 18:25:10 -0300

    up

--- aulas/s06e01/cobrinha/main.cpp
+++ aulas/s06e01/cobrinha/main.cpp
@@ -10,14 +10,15 @@ const int RIGHT = 1;
 const int DOWN = 2;
 const int LEFT = 3;
 
-const float casa = 50.0;
-//enum Dir {UP, RIGHT, DOWN, LEFL};
+const float casa = 50;
 
-float rotacao = 0;
+
+//enum Dir {UP, RIGHT, DOWN, LEFL};
 
 struct Par{
     int x;
     int y;
+    sf::Color cor;
 };
 
 struct Cobra{
@@ -26,12 +27,7 @@ struct Cobra{
 };
 
 void elemento_update(Cobra &cobra){
-
-    int tam = cobra.gomos.size();
-    for(int i = tam - 1; i >= 1; i--){
-        cobra.gomos[i] = cobra.gomos[i - 1];
-    }
-
+    Par aux = cobra.gomos[0];
     if(cobra.dir == UP)
         cobra.gomos[0].y -= 1;
     else if(cobra.dir == DOWN)
@@ -40,10 +36,16 @@ void elemento_update(Cobra &cobra){
         cobra.gomos[0].x -= 1;
     else if(cobra.dir == RIGHT)
         cobra.gomos[0].x += 1;
+
+    int tam = cobra.gomos.size();
+    for(int i = 1; i < tam; i++){
+        std::swap(aux.x, cobra.gomos[i].x);
+        std::swap(aux.y, cobra.gomos[i].y);
+    }
 }
 
-void elemento_controlar(Cobra &cobra, sf::Keyboard::Key tecla){
 
+void elemento_controlar(Cobra &cobra, sf::Keyboard::Key tecla){
     if(tecla == sf::Keyboard::Up)
         cobra.dir = UP;
     if(tecla == sf::Keyboard::Down)
@@ -55,18 +57,28 @@ void elemento_controlar(Cobra &cobra, sf::Keyboard::Key tecla){
 }
 
 int main(){
+    srand(time(NULL));
     sf::Clock relogio;
-    sf::Clock relogio_pirueta;
-    sf::RenderWindow janela(sf::VideoMode(1000, 600), "Janela");
+    sf::Clock relogio_maca;
 
-    sf::Texture tex_py;
-    tex_py.loadFromFile("../jararaca/pikachu.png");
-    sf::Sprite spr_py(tex_py);
-    spr_py.setScale(casa/tex_py.getSize().x, casa/tex_py.getSize().y);
+    int largura = 800;
+    int altura = 600;
 
+    int nlin = altura / casa;
+    int ncol = largura / casa;
+
+    sf::RenderWindow janela(sf::VideoMode(largura, altura), "Janela");
+
+    Par maca;
+    maca.x = rand() % ncol;
+    maca.y = rand() % nlin;
+    maca.cor = sf::Color(rand() % 255, rand() % 255, rand() % 255);
+
+    sf::RectangleShape rect;
+    rect.setSize(sf::Vector2f(casa - 4, casa - 4));
 
     Cobra cobra;//= {2, 2, DOWN};//inicializacao inline
-    cobra.gomos.push_back(Par{2, 2});
+    cobra.gomos.push_back(Par{2, 2, sf::Color::Black});
     cobra.dir = RIGHT;
 
     while(janela.isOpen()){
@@ -76,11 +88,24 @@ int main(){
                 janela.close();
             if(evento.type == sf::Event::KeyPressed){
                 elemento_controlar(cobra, evento.key.code);
-                if(evento.key.code == sf::Keyboard::C)
-                    cobra.gomos.push_back(cobra.gomos[0]);
             }
         }
 
+
+        if(cobra.gomos[0].x == maca.x && cobra.gomos[0].y == maca.y){
+            cobra.gomos.push_back(maca);
+            maca.x = rand() % ncol;
+            maca.y = rand() % nlin;
+            maca.cor = sf::Color(rand() % 255, rand() % 255, rand() % 255);
+            relogio_maca.restart();
+        }
+
+        if(relogio_maca.getElapsedTime() > sf::milliseconds(5000)){
+            relogio_maca.restart();
+            maca.x = rand() % ncol;
+            maca.y = rand() % nlin;
+            maca.cor = sf::Color(rand() % 255, rand() % 255, rand() % 255);
+        }
 
         if(relogio.getElapsedTime() > sf::milliseconds(250)){
             relogio.restart();
@@ -90,18 +115,23 @@ int main(){
         //fim
         janela.clear();
 
-        int i = rotacao;
-        for(auto par : cobra.gomos){
-            spr_py.setPosition(par.x * casa, par.y * casa);
-            spr_py.setOrigin(tex_py.getSize().x/2.0,
-                             tex_py.getSize().y/2.0);
-            spr_py.setRotation(rotacao);
-            janela.draw(spr_py);
-            i++;
+        rect.setFillColor(sf::Color(200, 100, 100));
+        for(int l = 0; l < nlin; l++){
+            for(int c = 0; c < ncol; c++){
+                rect.setPosition(c * casa + 2, l * casa + 2);
+                janela.draw(rect);
+            }
         }
-        if(relogio_pirueta.getElapsedTime() > sf::milliseconds(100)){
-            relogio_pirueta.restart();
-            rotacao += 10;
+
+        rect.setFillColor(maca.cor);
+        rect.setPosition(maca.x * casa + 2, maca.y * casa + 2);
+        janela.draw(rect);
+
+
+        for(auto gomo : cobra.gomos){
+            rect.setFillColor(gomo.cor);
+            rect.setPosition(gomo.x * casa + 2, gomo.y * casa + 2);
+            janela.draw(rect);
         }
         janela.display();
     }
