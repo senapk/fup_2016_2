7dc010bf116cb38511949d4c1526814d8090416a
David Sena <sena.ufc@gmail.com>
2016-09-21 15:36:40 -0300

    up

---
+++ aulas/s06e02/telefone.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+char transforma(char c){
+	string indices = "DGJMPTW";
+	for(int i = 0; i < indices.size(); i++)
+		if(indices[i] > c){
+			return (i + 2);
+}
+	
+int main(){
+	
+	string palavra;
+	cin >> palavra;	
+	for(char c : palavra){
+		if(c == '-')
+			cout << '-';
+		else if(c >= '0' && c <= '9')
+			cout << c;
+		else
+			cout << transforma(c);
+			
+			
+	}
+	return 0;
+}
+
+
+
+
+
+
